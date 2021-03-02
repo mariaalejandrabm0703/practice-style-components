e8bee34462142006b4e918fb859253acaf9079e6
Maria <mariaalejandrabm0703@gmail.com>
2021-03-02 13:10:58 -0500

    practica con styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import styled from 'styled-components';
+ 
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: ${props => (props.primary ? 'violet' : 'palevioletred')};
+  border: ${props =>
+    props.primary ? '2px solid violet' : '2px solid palevioletred'};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+ 
+  &:hover {
+    color: white;
+    background-color: ${props =>
+      props.primary ? 'violet' : 'palevioletred'};
+  }
+`;
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button>I am a Button</Button>
+      <Button primary>I am a Primary Button</Button>
     </div>
   );
 }
-
+ 
 export default App;
